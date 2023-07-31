62e348c28426657062f1cf6bd4ab708b6c9be0e4
TuckoxXL <sebastianbelluccini@gmail.com>
2023-07-31 02:31:43 -0300

    cositas

--- Assets/script/Bullet.cs
+++ Assets/script/Bullet.cs
@@ -10,7 +10,6 @@ public class Bullet : MonoBehaviour
     [SerializeField]
     private int bulletDamage = 10;
 
-    // Start is called before the first frame update
     void Start()
     {
         bullet = gameObject.GetComponent<Rigidbody2D>();
